5b16ac1300b5f59afda6a76ac0be835ebfa6bf73
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2021-01-17 20:02:46 -0500

    conditional searchbox and button styles

--- src/components/CustomSearchBox.js
+++ src/components/CustomSearchBox.js
@@ -8,41 +8,58 @@ const SearchBox = ({
   refine,
   placeholder,
   handleInputChange,
-}) => (
-  <form noValidate action="" role="search" className="searchbox">
-    <input
-      className="searchbox_input"
-      type="search"
-      value={currentRefinement}
-      onChange={(event) => {
-        handleInputChange(event.currentTarget.value);
-        refine(event.currentTarget.value);
-      }}
-      placeholder={placeholder}
-    />
-    <button
-      onClick={(e) => {
-        e.preventDefault();
-        refine("");
-      }}
-      className="searchbox_button"
-    >
-      <div
-        style={{
-          width: "75%",
-          margin: "auto",
+}) => {
+  const conditionalInputStyles = !currentRefinement
+    ? {
+        borderBottom: "none",
+        borderRadius: "none",
+        borderBottomLeftRadius: "25px",
+      }
+    : {};
+  const conditionalBtnStyles = !currentRefinement
+    ? {
+        borderBottomRightRadius: "25px",
+      }
+    : {};
+
+  return (
+    <form noValidate action="" role="search" className="searchbox">
+      <input
+        style={conditionalInputStyles}
+        className="searchbox_input"
+        type="search"
+        value={currentRefinement}
+        onChange={(event) => {
+          handleInputChange(event.currentTarget.value);
+          refine(event.currentTarget.value);
         }}
+        placeholder={placeholder}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          refine("");
+        }}
+        style={conditionalBtnStyles}
+        className="searchbox_button"
       >
-        <img
-          style={{ width: "100%", height: "100%" }}
-          src={searchIcon}
-          alt=""
-        />
-      </div>
-    </button>
-    {isSearchStalled ? "My search is stalled" : ""}
-  </form>
-);
+        <div
+          style={{
+            width: "75%",
+            margin: "auto",
+          }}
+        >
+          <img
+            style={{ width: "100%", height: "100%" }}
+            src={searchIcon}
+            alt=""
+          />
+        </div>
+      </button>
+      {isSearchStalled ? "My search is stalled" : ""}
+    </form>
+  );
+};
 
 const CustomSearchBox = connectSearchBox(SearchBox);
 
