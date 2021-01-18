3fe5a295713d58e836b4eec3fbecf010a123237d
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2021-01-18 12:13:23 -0500

    custom scroll bar

--- src/components/CustomHits.js
+++ src/components/CustomHits.js
@@ -1,12 +1,16 @@
 import { connectHits, Stats } from "react-instantsearch-dom";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 import Hit from "./Hit";
 
 const Hits = ({ hits, hitsPossible }) => (
   <div className="hits_container">
-    {hitsPossible && <Stats />}
-    {hits.map((hit) => (
-      <Hit key={hit.objectID} hit={hit} />
-    ))}
+    <SimpleBar style={{ maxHeight: "60vh" }}>
+      {hitsPossible && <Stats />}
+      {hits.map((hit) => (
+        <Hit key={hit.objectID} hit={hit} />
+      ))}
+    </SimpleBar>
   </div>
 );
 
