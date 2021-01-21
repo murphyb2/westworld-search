1b33629edde44b1c14459e1ae72a471866ee94a6
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2021-01-21 15:49:15 -0500

    fetch episode detail data after refresh

--- src/components/EpisodeDetail.js
+++ src/components/EpisodeDetail.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Redirect, Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { ReactComponent as PlusIcon } from "../assets/plus.svg";
-import { useEpisode } from "../context/EpisodeContext";
+import { useEpisode, useFindEpisode } from "../context/EpisodeContext";
 import ImageLoad from "./ImageLoad";
+import spinner from "../assets/spinner-green.svg";
 
 const EpisodeDetail = () => {
   const episode = useEpisode();
+  const { objectID: id } = useParams();
+  const findEpisode = useFindEpisode();
 
-  return episode.objectID ? (
+  if (!episode.objectID) findEpisode(id);
+
+  return (
     <div
       className="container"
       style={{
@@ -28,53 +33,75 @@ const EpisodeDetail = () => {
           </h5>
         </button>
       </Link>
-      <h1 className="episodeDetail_title">{episode.name}</h1>
-      <div className="episodeDetail_content">
-        <ImageLoad
-          src={episode.image.original}
-          placeholder={episode.image.medium}
-          alt=""
-        />
-        <div className="episodeDetail_body">
-          <h2>{episode.summary}</h2>
-          <div style={{ margin: "1.5rem 0" }}>
-            Season {episode.season} / Episode {episode.episode}
+      {episode.objectID ? (
+        <>
+          <h1 className="episodeDetail_title">{episode.name}</h1>
+          <div className="episodeDetail_content">
+            <ImageLoad
+              src={episode.image.original}
+              placeholder={episode.image.medium}
+              alt=""
+            />
+            <div className="episodeDetail_body">
+              <h2>{episode.summary}</h2>
+              <div style={{ margin: "1.5rem 0" }}>
+                Season {episode.season} / Episode {episode.episode}
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  fontSize: "large",
+                }}
+              >
+                <p>Airdate: {new Date(episode.airdate).toDateString()}</p>
+                <p>Runtime: {episode.runtime} mins</p>
+              </div>
+            </div>
           </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              fontSize: "large",
-            }}
-          >
-            <p>Airdate: {new Date(episode.airdate).toDateString()}</p>
-            <p>Runtime: {episode.runtime} mins</p>
-          </div>
-        </div>
-      </div>
-      <a href={episode.url} target="_blank" rel="noreferrer">
-        <button
+          <a href={episode.url} target="_blank" rel="noreferrer">
+            <button
+              style={{
+                margin: "6vh auto auto auto",
+                backgroundColor: "var(--grey)",
+                border: "none",
+                color: "white",
+                padding: "0.5rem 0.75rem",
+                display: "flex",
+                justifyContent: "space-between",
+                borderRadius: "12px",
+              }}
+              className="btn"
+            >
+              <h2 style={{ margin: "auto 0", paddingRight: ".75rem" }}>
+                More Episode Detail
+              </h2>
+              <PlusIcon />
+            </button>
+          </a>
+        </>
+      ) : (
+        <div
           style={{
-            margin: "6vh auto auto auto",
-            backgroundColor: "var(--grey)",
-            border: "none",
-            color: "white",
-            padding: "0.5rem 0.75rem",
+            padding: "2rem",
+            margin: "2rem",
             display: "flex",
-            justifyContent: "space-between",
-            borderRadius: "12px",
+            justifyContent: "center",
+            overflow: "hidden",
           }}
-          className="btn"
         >
-          <h2 style={{ margin: "auto 0", paddingRight: ".75rem" }}>
-            More Episode Detail
-          </h2>
-          <PlusIcon />
-        </button>
-      </a>
+          <img
+            className="loader"
+            style={{
+              width: "25%",
+              height: "auto",
+            }}
+            src={spinner}
+            alt=""
+          />
+        </div>
+      )}
     </div>
-  ) : (
-    <Redirect to="/" />
   );
 };
 
--- src/context/EpisodeContext.js
+++ src/context/EpisodeContext.js
@@ -1,20 +1,38 @@
 import React, { useState, createContext, useContext } from "react";
+import algoliasearch from "algoliasearch/lite";
+
+const searchClient = algoliasearch(
+  process.env.REACT_APP_ALGOLIA_APP_ID,
+  process.env.REACT_APP_ALGOLIA_ADMIN_KEY
+);
+
+const index = searchClient.initIndex(process.env.REACT_APP_ALGOLIA_INDEX);
 
 const EpisodeContext = createContext();
 const EpisodeUpdateContext = createContext();
+const FindEpisodeContext = createContext();
 
 export const useEpisode = () => useContext(EpisodeContext);
 export const useEpisodeUpdate = () => useContext(EpisodeUpdateContext);
+export const useFindEpisode = () => useContext(FindEpisodeContext);
 
 const EpisodeProvider = ({ children }) => {
   const [episode, setEpisode] = useState({});
 
-  const updateEpisode = (ep) => setEpisode(ep);
+  const updateEpisode = ep => setEpisode(ep);
+  const findEpisode = async id => {
+    const ep = await index.search("", {
+      facetFilters: `objectID:${id}`,
+    });
+    setEpisode(ep.hits[0]);
+  };
 
   return (
     <EpisodeContext.Provider value={episode}>
       <EpisodeUpdateContext.Provider value={updateEpisode}>
-        {children}
+        <FindEpisodeContext.Provider value={findEpisode}>
+          {children}
+        </FindEpisodeContext.Provider>
       </EpisodeUpdateContext.Provider>
     </EpisodeContext.Provider>
   );
